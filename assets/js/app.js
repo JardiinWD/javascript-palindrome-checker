79460a55fae82aa4d49747e303ab5cebfcd2220c
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-23 22:19:48 +0200

    Aggiunto condizione per verificare input utente

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,24 +2,35 @@ const textInput = document.querySelector(".inputs input") // Prendo il mio tag i
 checkBtn = document.querySelector(".inputs button") // Dalla classe inputs prendo il mio button per verificare il value dall'input
 console.log(textInput); // in effetti rilevo che ho preso "<input type="text" placeholder="Inserisci testo o numeri">""
 
+
+/* Scateno evento anche sul button del verifica */
+checkBtn.addEventListener("click", () => {
+    /* console.log("Sono il pulsante Verifica e mi stai cliccando"); */
+})
+
+
+
 /* Scateno un evento sull'input del mio utente, alla digitazione "keyup" ascolto cosa sta scrivendo */
 textInput.addEventListener("keyup", () => {
     let filtro = textInput.value.replace(/[^A-Z0-9]/ig, "") // Nel mio filtro mi assicuro di rimuovere simboli perc, spazzi, diviso e cosi via 
     console.log(filtro);
-    /* Se il valore input ha dei caratteri al suo interno */
-    if (filtro) {
+    /* Se il valore input ha dei caratteri al suo interno e soprattutto se la lunghezza della parola è più lunga di 2 */
+    if (filtro && filtro.length >= 2) {
+        /* console.log(`Sono la lunghezza dell'input ${filtro.length}`); */
         // Aggiungo la classe active solo se nel filtro è presente un carattere
         return checkBtn.classList.add("active")
-    } else {
+    }
+    else {
         /* Altrimenti al bottone rimuovi la classe active */
         checkBtn.classList.remove("active")
     }
-
     /*     
     console.log("Sono il testo digitato dall'utente");
     console.log(textInput.value); 
     */
 })
+
+
 
 
 
